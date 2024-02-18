ba31ef2fc50036573cf61493cb1c0eed9681cb44
McVader34 <130928541+McVader34@users.noreply.github.com>
2024-02-18 12:28:01 -0500

    Delete src/main directory

--- src/main/java/net/mcvader/seriousplayeranimations/IExampleAnimatedPlayer.java
+++
@@ -1,9 +0,0 @@
-package net.mcvader.seriousplayeranimations;
-
-import dev.kosmx.playerAnim.api.layered.IAnimation;
-import dev.kosmx.playerAnim.api.layered.ModifierLayer;
-
-public interface IExampleAnimatedPlayer {
-
-    ModifierLayer<IAnimation> seriousplayeranimations_getModAnimation();
-}
