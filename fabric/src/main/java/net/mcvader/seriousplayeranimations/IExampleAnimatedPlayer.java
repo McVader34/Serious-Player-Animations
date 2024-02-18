15ed175d4f5b4c5a3634c27ed8c88a106c76e5f7
McVader34 <130928541+McVader34@users.noreply.github.com>
2024-02-18 12:46:25 -0500

    Add files via upload

---
+++ fabric/src/main/java/net/mcvader/seriousplayeranimations/IExampleAnimatedPlayer.java
@@ -0,0 +1,9 @@
+package net.mcvader.seriousplayeranimations;
+
+import dev.kosmx.playerAnim.api.layered.IAnimation;
+import dev.kosmx.playerAnim.api.layered.ModifierLayer;
+
+public interface IExampleAnimatedPlayer {
+
+    ModifierLayer<IAnimation> seriousplayeranimations_getModAnimation();
+}
